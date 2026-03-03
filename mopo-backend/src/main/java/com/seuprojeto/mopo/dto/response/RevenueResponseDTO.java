d8fbf6565bf7fc0e0389e677177bbc15cd5bbb4a
Guilherme <guilhermesantanapessa12@gmail.com>
2026-03-03 13:47:25 -0300

    feat: adiciona RevenueResponseDTO

---
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/dto/response/RevenueResponseDTO.java
@@ -0,0 +1,36 @@
+package com.seuprojeto.mopo.dto.response;
+
+import com.seuprojeto.mopo.model.Revenue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record RevenueResponseDTO(
+        UUID id,
+        String title,
+        String description,
+        String image,
+        List<String> ingredients,
+        List<String> instructions,
+        int preparationTime,
+        int efficiency,
+        int rating,
+        int difficulty
+) {
+
+    public static RevenueResponseDTO fromEntity(Revenue revenue) {
+        return new RevenueResponseDTO(
+                revenue.getId(),
+                revenue.getTitle(),
+                revenue.getDescription(),
+                revenue.getImage(),
+                revenue.getIngredients(),
+                revenue.getInstructions(),
+                revenue.getPreparationTime(),
+                revenue.getEfficiency(),
+                revenue.getRating(),
+                revenue.getDifficulty()
+        );
+    }
+}
