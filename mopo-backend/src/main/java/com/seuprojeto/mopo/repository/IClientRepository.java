d3a3d42e4150c747c7c36c47d8ada3aef6cca583
Guilherme <guilhermesantanapessa12@gmail.com>
2026-03-03 15:10:58 -0300

    feat: adiciona findAll com Pageable nas interfaces IRevenueRepository e IClientRepository

--- mopo-backend/src/main/java/com/seuprojeto/mopo/repository/IClientRepository.java
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/repository/IClientRepository.java
@@ -1,6 +1,8 @@
 package com.seuprojeto.mopo.repository;
 
 import com.seuprojeto.mopo.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface IClientRepository extends JpaRepository<Client, UUID> {
+    Page<Client> findAll(Pageable pageable);
 }
