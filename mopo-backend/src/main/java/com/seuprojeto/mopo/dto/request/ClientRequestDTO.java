57c09f003bf9de65839d8b11f0a00cf50315b3d1
Guilherme <guilhermesantanapessa12@gmail.com>
2026-03-03 01:10:14 -0300

    refactor: renomeia mensagens

--- mopo-backend/src/main/java/com/seuprojeto/mopo/dto/request/ClientRequestDTO.java
+++ mopo-backend/src/main/java/com/seuprojeto/mopo/dto/request/ClientRequestDTO.java
@@ -1,21 +1,27 @@
 package com.seuprojeto.mopo.dto.request;
 
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
-
 public record ClientRequestDTO(
-        @NotBlank(message = "Username não pode estar vazio") String username,
+        @NotBlank(message = "The username cannot be empty.")
+        String username,
 
-        @Email(message = "Email deve ser válido") @NotBlank(message = "Email é obrigatório") String email,
+        @Email(message = "The email address must be valid.")
+        @NotBlank(message = "Email is required.")
+        String email,
 
-        @NotBlank(message = "Senha é obrigatória") @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres") String password,
+        @NotBlank(message = "A password is required.")
+        @Size(min = 6, message = "The password must be at least 6 characters long.")
+        String password,
 
-        @NotBlank(message = "CPF é obrigatório") @Size(min = 11, max = 11, message = "CPF deve ter 11 dígitos") String cpf,
+        @NotBlank(message = "CPF is required.")
+        @Size(min = 11, max = 11, message = "CPF must have 11 digits.")
+        String cpf,
 
-        @NotNull(message = "Data de nascimento é obrigatória") @Past(message = "Data de nascimento inválida") LocalDate birthday) {
+        @NotNull(message = "Date of birth is required.")
+        @Past(message = "Invalid date of birth")
+        LocalDate birthday
+) {
 }
